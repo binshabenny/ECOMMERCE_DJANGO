c9b7a998cae2c8e6def56982f94e5f1aa665a40a
Binsha Benny <binshabenny3@gmail.com>
2024-01-10 12:13:23 +0530

    cart added in the navbar

--- home/urls.py
+++ home/urls.py
@@ -17,8 +17,10 @@ urlpatterns = [
     path('view_cart/', views.view_cart, name='view_cart'),
     path('cart/', views.cart, name='cart'),
     path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
-    path("cart/cart_detail/", views.cart_detail, name="cart_detail"),
+    path("cart/cart_detail", views.cart_detail, name="cart_detail"),
     path("remove_from_cart/<int:product_id>", views.remove_from_cart, name="remove_from_cart"),
+    path('cart/',views.cart,name="cart"),
+
  
 
 ]
